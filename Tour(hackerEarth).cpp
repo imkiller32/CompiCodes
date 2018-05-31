fc34cf00421ca66de42c7cc95bf339acc34ec5ba
Ritesh Aggarwal <riteshagg50297@gmail.com>
2018-05-31 13:24:08 +0530

    Simple Map
    ProblemLink-->https://www.hackerearth.com/practice/algorithms/string-algorithm/z-algorithm/practice-problems/algorithm/tour/

---
+++ Tour(hackerEarth).cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+ll cost[1001][1001];
+
+int main()
+{
+	ll n,p,i,j,sum=0,now;
+	string s;
+	map<string,ll> m;
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cin>>s;
+		m[s]=i;
+	}
+	for(i=0;i<n;i++)
+		for(j=0;j<n;j++)
+			cin>>cost[i][j];			
+	cin>>p;
+	j=0;
+	for(i=0;i<p;i++)
+	{
+		cin>>s;
+		now=m[s];
+		sum+=cost[j][now];
+		j=now;
+	}
+	cout<<sum<<endl;
+}
